fc039be4f6edcda9bf8643ef89d24562f9f6d33c
Leila Angela Arcega <Leila.Arcega@wizard-ai.com>
2024-12-20 13:58:43 +0800

    Update portfolio

--- javascript/script.js
+++ javascript/script.js
@@ -58,4 +58,54 @@ function slideButtons(){
   }
 }
 
+const modal = document.getElementById('imageModal');
+const modalImg = document.getElementById('modalImage');
+const captionText = document.getElementById('caption');
+const closeModal = document.getElementById('closeModal');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+closeModal.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
+
+function handleClick(event) {
+  const clickedElementId = event.target.id;
+  console.log(clickedElementId);
+  const thumbnail = document.getElementById(`${clickedElementId}`);
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+const image = document.querySelectorAll(".flex-img");
+image.forEach(image => {
+  image.addEventListener("click", handleClick);
+});
+
+
+const triggerElement = document.querySelector('.intro');
+const elementToChange = document.querySelector('.intro-container');
+const elementToChange2 = document.querySelector('body');
+
+triggerElement.addEventListener('mouseenter', function() {
+    elementToChange.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+    elementToChange2.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+});
+
+
+triggerElement.addEventListener('mouseleave', function() {
+    elementToChange.style.backgroundColor = '#e7e7e7';
+    elementToChange2.style.backgroundColor = '#e7e7e7';
+});
+
+
+
 slideButtons();
